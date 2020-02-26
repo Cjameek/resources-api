4463dbced6c50a634f6d8f1b8d2720cf36562d9d
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-02-26 17:05:56 -0500

    Added response logic

--- api/routes/resources.js
+++ api/routes/resources.js
@@ -5,8 +5,18 @@ const mongoose = require('mongoose');
 const Resource = require('../models/resources');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET resource route'
+    Resource.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
@@ -18,38 +28,66 @@ router.post('/', (req, res, next) => {
     });
     resource.save().then(result => {
         console.log(result);
+        res.status(201).json({
+            message: 'Handling POST resource route',
+            createdResource: resource
+        });
     })
-    .catch(err => console.log(err));
-    
-    res.status(201).json({
-        message: 'Handling POST resource route',
-        createdResource: resource
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
 router.get('/:resourceId', (req, res, next) => {
     const id = req.params.resourceId;
-    if(id === 'special'){
-        res.status(200).json({
-            message: 'Individual resource GET',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
+    Resource.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'No valid entry found'
+            })
+        }
+    })
+    .catch( err => { 
+        console.log(err)
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch('/:resourceId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated resource'
+    const id = req.params.resourceId;
+    Resource.updateOne({_id: id}, {$set: req.body})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: 'Resource updated successfully'
+        });
+    })
+    .catch( err => { 
+        console.log(err)
+        res.status(500).json({error: err});
     });
 });
 
 router.delete('/:resourceId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted resource'
+    const id = req.params.resourceId;
+    Resource.remove({_id: id})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch( err => { 
+        console.log(err)
+        res.status(500).json({error: err});
     });
 });
 
