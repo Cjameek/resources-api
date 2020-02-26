c2c047c8805daa43b90d3dcc04fe9f941e2af454
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-02-25 19:15:10 -0500

    Added other method routes

--- api/routes/resources.js
+++ api/routes/resources.js
@@ -8,8 +8,40 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const resource = {
+        name: req.body.name,
+        category: req.body.category
+    }
+    
+    res.status(201).json({
+        message: 'Handling POST resource route',
+        createdResource: resource
+    });
+});
+
+router.get('/:resourceId', (req, res, next) => {
+    const id = req.params.resourceId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'Individual resource GET',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:resourceId', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling POST resource route'
+        message: 'Updated resource'
+    });
+});
+
+router.delete('/:resourceId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted resource'
     });
 });
 
