4f11c9646eba751e5b7c5a99caf9739f1ddd53f5
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-02-25 19:14:54 -0500

    Header, error & body handling

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const resourceRoutes = require('./api/routes/resources');
+const userRoutes = require('./api/routes/users');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Header handling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Handle request routes
 app.use('/resources', resourceRoutes);
+app.use('/users', userRoutes);
+
+// Error handling on bad REST routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// Error handling in event of database errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
