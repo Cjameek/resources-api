d95b275dc7bf9b8d9245e99092afb0f24afd68b8
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-02-29 07:46:47 -0500

    Resources support file in schema

--- api/models/resources.js
+++ api/models/resources.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const resourceSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-    category: String
+    category: String,
+    resourceFile: String
 })
 
 module.exports = mongoose.model('Resource', resourceSchema);
