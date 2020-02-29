b65c3b2adac39a9dd7ad2425956fa3ded3d1e05c
Cody Meek <codymeek@Codys-MacBook-Pro-2.local>
2020-02-29 07:46:26 -0500

    Added support for file uploading

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 // Handle request routes
+app.use('/uploads', express.static('uploads'));
 app.use('/resources', resourceRoutes);
 app.use('/users', userRoutes);
 
